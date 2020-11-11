c7169c39ca592c6fb7aef53e7fe784879937fd4a
Guy Boldon <gb@guyboldon.com>
2020-11-11 23:20:32 +0100

    implement custom exception

---
+++ src/main/kotlin/com/guyboldon/configuration/AffirmationException.kt
@@ -0,0 +1,6 @@
+package com.guyboldon.configuration
+
+class AffirmationException : RuntimeException {
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+}
